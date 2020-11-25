18c1d92c84e45930c5233d7dabf80fb84e829600
shamilbi <shamilbi@gmail.com>
2020-11-25 16:54:14 +0700

    pylint, main() --> main.py
    selecta/__init__.py
        class LineCountWidget(urwid.Text):
        +    def __init__(self, text: str):
        +        self.relevant_lines = 0
        +        self.visible_lines = 0
        +        super().__init__(text)
    
        class Selector:
        def on_unhandled_input(self, input_):
                     try:
                         line = self.listbox.get_focus()[0].line
                     except AttributeError:  # empty list
        -                return
        +                return False
    
        -def main(): --> main.py
    setup.py
        -    entry_points={'console_scripts': ['selecta = selecta.__init__:main']},
        +    entry_points={'console_scripts': ['selecta = selecta.main:main']},

--- selecta/__init__.py
+++ selecta/__init__.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+# coding: utf-8
+
 import fcntl
 import termios
 import sys
@@ -10,7 +11,7 @@ import argparse
 # from subprocess import call
 import urwid
 
-# pylint: disable=line-too-long
+# pylint: disable=line-too-long,unused-argument
 
 if sys.version_info < (3, 0):
     sys.exit('Sorry, you need Python 3 to run this!')
@@ -137,6 +138,11 @@ class ResultList(urwid.ListBox):
 
 
 class LineCountWidget(urwid.Text):
+    def __init__(self, text: str):
+        self.relevant_lines = 0
+        self.visible_lines = 0
+        super().__init__(text)
+
     def update(self, relevant_lines=None, visible_lines=None):
         if not hasattr(self, 'relevant_lines'):
             self.relevant_lines = 0
@@ -314,7 +320,7 @@ class Selector:
             try:
                 line = self.listbox.get_focus()[0].line
             except AttributeError:  # empty list
-                return
+                return False
 
             self.view.set_header(urwid.AttrMap(
                 urwid.Text('selected: {}'.format(line)), 'head'))
@@ -366,50 +372,3 @@ class Selector:
         for c in command:
             fcntl.ioctl(fd, termios.TIOCSTI, c)
         termios.tcsetattr(fd, termios.TCSANOW, old)
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--revert-order', action='store_true', default=False, help='revert the order of the lines')
-    parser.add_argument('-b', '--remove-bash-prefix', action='store_true', default=False, help='remove the numeric prefix from bash history')
-    parser.add_argument('-z', '--remove-zsh-prefix', action='store_true', default=False, help='remove the time prefix from zsh history')
-    parser.add_argument('-e', '--regexp', action='store_true', default=False, help='start in regexp mode')
-    parser.add_argument('-a', '--case-sensitive', action='store_true', default=True, help='start in case-sensitive mode')
-    parser.add_argument('-d', '--remove-duplicates', action='store_true', default=False, help='remove duplicated lines')
-    parser.add_argument('-y', '--show-matches', action='store_true', default=False, help='highlight the part of each line which match the substrings or regexp')
-    parser.add_argument('--bash', action='store_true', default=False, help='standard for bash history search, same as -b -i -d')
-    parser.add_argument('--zsh', action='store_true', default=False, help='standard for zsh history search, same as -b -i -d')
-    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='the file which lines you want to select eg. <(history)')
-
-    args = parser.parse_args()
-
-    if args.infile.name == '<stdin>':
-        parser.print_help()
-        sys.exit('\nYou must provide an infile!')
-
-    if args.bash:
-        args.revert_order = True
-        args.remove_bash_prefix = True
-        args.remove_duplicates = True
-
-    if args.zsh:
-        args.revert_order = True
-        args.remove_zsh_prefix = True
-        args.remove_duplicates = True
-
-    Selector(
-        revert_order=args.revert_order,
-        remove_bash_prefix=args.remove_bash_prefix,
-        remove_zsh_prefix=args.remove_zsh_prefix,
-        regexp=args.regexp,
-        case_sensitive=args.case_sensitive,
-        remove_duplicates=args.remove_duplicates,
-        show_matches=args.show_matches,  # TODO highlight more than one part
-        infile=args.infile,
-        # TODO support missing options
-    )
-
-
-if __name__ == '__main__':
-    main()
-    # version bump
---
+++ selecta/main.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+
+import sys
+import argparse
+from selecta import Selector
+
+# pylint: disable=line-too-long
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--revert-order', action='store_true', default=False, help='revert the order of the lines')
+    parser.add_argument('-b', '--remove-bash-prefix', action='store_true', default=False, help='remove the numeric prefix from bash history')
+    parser.add_argument('-z', '--remove-zsh-prefix', action='store_true', default=False, help='remove the time prefix from zsh history')
+    parser.add_argument('-e', '--regexp', action='store_true', default=False, help='start in regexp mode')
+    parser.add_argument('-a', '--case-sensitive', action='store_true', default=True, help='start in case-sensitive mode')
+    parser.add_argument('-d', '--remove-duplicates', action='store_true', default=False, help='remove duplicated lines')
+    parser.add_argument('-y', '--show-matches', action='store_true', default=False, help='highlight the part of each line which match the substrings or regexp')
+    parser.add_argument('--bash', action='store_true', default=False, help='standard for bash history search, same as -b -i -d')
+    parser.add_argument('--zsh', action='store_true', default=False, help='standard for zsh history search, same as -b -i -d')
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='the file which lines you want to select eg. <(history)')
+
+    args = parser.parse_args()
+
+    if args.infile.name == '<stdin>':
+        parser.print_help()
+        sys.exit('\nYou must provide an infile!')
+
+    if args.bash:
+        args.revert_order = True
+        args.remove_bash_prefix = True
+        args.remove_duplicates = True
+
+    if args.zsh:
+        args.revert_order = True
+        args.remove_zsh_prefix = True
+        args.remove_duplicates = True
+
+    Selector(
+        revert_order=args.revert_order,
+        remove_bash_prefix=args.remove_bash_prefix,
+        remove_zsh_prefix=args.remove_zsh_prefix,
+        regexp=args.regexp,
+        case_sensitive=args.case_sensitive,
+        remove_duplicates=args.remove_duplicates,
+        show_matches=args.show_matches,  # TODO highlight more than one part
+        infile=args.infile,
+        # TODO support missing options
+    )
+
+
+if __name__ == '__main__':
+    main()
+    # version bump
